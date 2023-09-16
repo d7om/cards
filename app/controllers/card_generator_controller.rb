c4d087164d9f1f01ee5cc37d0bda86da52d5d288
Abdulrahman Alshetwi <dhom@shetwi.com>
2023-09-16 18:48:12 +0300

    First working verion

--- app/controllers/card_generator_controller.rb
+++ app/controllers/card_generator_controller.rb
@@ -1,24 +1,48 @@
 class CardGeneratorController < ApplicationController
-    def new
-      # Display a form for users to enter their names
-    end
-  
-    def create
-      user_name = params[:user_name]
-      template_path = params[:template_path] # Path to the selected card template
-  
-      # Load the card template image using RMagick
-      template = Magick::Image.read(template_path).first
-  
-      # Add the user's name to the image
-      draw = Magick::Draw.new
-      draw.annotate(template, 0, 0, x, y, user_name) do
-        self.font = 'path/to/your/font.ttf' # Replace with the path to your font
-        self.pointsize = 36
-        self.fill = 'white'
-      end
-  
-      # Serve the generated card to the user
-      send_data template.to_blob, filename: "congratulations_card.png", type: 'image/png', disposition: 'inline'
-    end
+  def new
+    # Display a form for users to enter their names and select a card template
   end
+
+  def create
+
+    selected_image_path = params[:card_order][:template_path]
+    user_name = params[:user_name]
+    template_path = params[:card_order][:template_path]
+
+    # Load the card template image using RMagick
+    template = Magick::Image.read(template_path).first
+
+    # Add the user's name to the image with the correct font and size
+    draw = Magick::Draw.new
+    draw.annotate(template, 800, 500, 0, 0, user_name) do |card|
+      card.font = 'Cairo-VariableFont_slnt,wght.ttf'
+      card.pointsize = 36         # Specify the font size
+      card.fill = 'white'        # Specify the font color
+      card.gravity = Magick::CenterGravity # Position the text at the center
+    end
+
+
+   # Create a unique filename for the generated image
+   file_name = "congratulations_card_#{Time.now.to_i}.png"
+
+   # Save the generated image to the public directory
+   file_path = Rails.root.join('public', file_name)
+   template.write(file_path)
+    
+   # Redirect to show with param file_name
+   redirect_to action: "show", file_name: file_name
+
+end
+
+def show
+  file_path = Rails.root.join('public', params[:file_name])
+
+  if File.exist?(file_path)
+    send_file file_path, type: 'image/png', disposition: 'inline'
+  else
+    flash[:error] = "Image not found"
+    redirect_to root_path
+  end
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'card_generator#new'
-  post 'card_generator/generate_card', to: 'card_generator#create', as: 'generate_card'
+  get 'card_generator', to: 'card_generator#new'
+  post 'card_generator', to: 'card_generator#create', as: 'generate_card'
+  get 'card_generator/show/', to: 'card_generator#show', as: "show"
+  post 'card_generator/show/:file_path', to: 'card_generator#show', as: :show_generated_card
 end
